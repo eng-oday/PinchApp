1fe88a50770178fbed746ca6962ac39e452bcb27
eng-oday <odaydieg@gmail.com>
2023-07-25 13:56:33 +0300

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by 3rabApp-oday on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
